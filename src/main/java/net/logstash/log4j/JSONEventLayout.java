8d1df5a32167e5225f10a851582a7d56cbb0940f
Pierre-Yves Ritschard <pyr@spootnik.org>
2012-12-08 09:16:31 +0100

    format dates as ISO8601

--- src/main/java/net/logstash/log4j/JSONEventLayout.java
+++ src/main/java/net/logstash/log4j/JSONEventLayout.java
@@ -3,6 +3,8 @@ package net.logstash.log4j;
 import net.logstash.log4j.data.HostData;
 
 import java.util.*;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 
 import net.minidev.json.JSONObject;
 import org.apache.commons.lang.*;
@@ -27,6 +29,21 @@ public class JSONEventLayout extends Layout {
 
     private JSONObject logstashEvent;
 
+    public static String dateFormat(long timestamp) {
+	Date date = new Date(timestamp);
+	/*
+	 * SimpleDateFormat isn't thread safe so I need one 
+	 * instance per call, otherwise I'd have to pull in
+	 * joda time.
+	 */
+	SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssZ");
+	String formatted = format.format(date);
+
+	/* 
+	 * No native support for ISO8601 woo!
+	 */
+	return formatted.substring(0,22) + ":" + formatted.substring(22);
+    }
 
     public String format(LoggingEvent loggingEvent) {
         hostname = new HostData().getHostName();
@@ -69,7 +86,7 @@ public class JSONEventLayout extends Layout {
         addFieldData("mdc",mdc);
         addFieldData("ndc",ndc);
         addFieldData("level",loggingEvent.getLevel().toString());
-        addFieldData("timestamp",timestamp);
+        addFieldData("timestamp",dateFormat(timestamp));
 
         logstashEvent.put("@fields",fieldData);
         return logstashEvent.toString() + "\n";
