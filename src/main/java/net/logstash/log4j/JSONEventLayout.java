63a59e61b2239de78a7d7073709b7025f7752ae7
Christophe Furmaniak <christophe.furmaniak@gmail.com>
2013-04-01 18:50:18 +0200

    organise imports and format code

--- src/main/java/net/logstash/log4j/JSONEventLayout.java
+++ src/main/java/net/logstash/log4j/JSONEventLayout.java
@@ -1,17 +1,17 @@
 package net.logstash.log4j;
 
 import net.logstash.log4j.data.HostData;
-
-import java.util.*;
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
-
 import net.minidev.json.JSONObject;
-import org.apache.commons.lang.*;
+import org.apache.commons.lang.StringUtils;
 import org.apache.log4j.Layout;
+import org.apache.log4j.spi.LocationInfo;
 import org.apache.log4j.spi.LoggingEvent;
 import org.apache.log4j.spi.ThrowableInformation;
-import org.apache.log4j.spi.LocationInfo;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 public class JSONEventLayout extends Layout {
 
@@ -21,7 +21,7 @@ public class JSONEventLayout extends Layout {
     private boolean ignoreThrowable = false;
 
     private boolean activeIgnoreThrowable = ignoreThrowable;
-    private String hostname = new HostData().getHostName();;
+    private String hostname = new HostData().getHostName();
     private long timestamp;
     private String ndc;
     private Map mdc;
@@ -38,17 +38,17 @@ public class JSONEventLayout extends Layout {
     };
 
     public static String dateFormat(long timestamp) {
-	Date date = new Date(timestamp);
-	/*
+        Date date = new Date(timestamp);
+    /*
 	 * SimpleDateFormat isn't thread safe so we use a ThreadLocal,
 	 *  otherwise we'd have to pull in joda time.
 	 */
-	String formatted = THREAD_LOCAL_SDF.get().format(date);
+        String formatted = THREAD_LOCAL_SDF.get().format(date);
 
 	/* 
 	 * No native support for ISO8601 woo!
 	 */
-	return formatted.substring(0,26) + ":" + formatted.substring(26);
+        return formatted.substring(0, 26) + ":" + formatted.substring(26);
     }
 
     /**
@@ -77,38 +77,38 @@ public class JSONEventLayout extends Layout {
 
         logstashEvent = new JSONObject();
 
-        logstashEvent.put("@source_host",hostname);
-        logstashEvent.put("@message",loggingEvent.getRenderedMessage());
-        logstashEvent.put("@timestamp",dateFormat(timestamp));
+        logstashEvent.put("@source_host", hostname);
+        logstashEvent.put("@message", loggingEvent.getRenderedMessage());
+        logstashEvent.put("@timestamp", dateFormat(timestamp));
 
-        if(loggingEvent.getThrowableInformation() != null) {
+        if (loggingEvent.getThrowableInformation() != null) {
             final ThrowableInformation throwableInformation = loggingEvent.getThrowableInformation();
-            if(throwableInformation.getThrowable().getClass().getCanonicalName() != null){
-                exceptionInformation.put("exception_class",throwableInformation.getThrowable().getClass().getCanonicalName());
+            if (throwableInformation.getThrowable().getClass().getCanonicalName() != null) {
+                exceptionInformation.put("exception_class", throwableInformation.getThrowable().getClass().getCanonicalName());
             }
-            if(throwableInformation.getThrowable().getMessage() != null) {
-                exceptionInformation.put("exception_message",throwableInformation.getThrowable().getMessage());
+            if (throwableInformation.getThrowable().getMessage() != null) {
+                exceptionInformation.put("exception_message", throwableInformation.getThrowable().getMessage());
             }
-            if( throwableInformation.getThrowableStrRep() != null) {
-                String stackTrace = StringUtils.join(throwableInformation.getThrowableStrRep(),"\n");
-                exceptionInformation.put("stacktrace",stackTrace);
+            if (throwableInformation.getThrowableStrRep() != null) {
+                String stackTrace = StringUtils.join(throwableInformation.getThrowableStrRep(), "\n");
+                exceptionInformation.put("stacktrace", stackTrace);
             }
-            addFieldData("exception",exceptionInformation);
+            addFieldData("exception", exceptionInformation);
         }
 
-        if(locationInfo) {
+        if (locationInfo) {
             info = loggingEvent.getLocationInformation();
-            addFieldData("file",info.getFileName());
-            addFieldData("line_number",info.getLineNumber());
-            addFieldData("class",info.getClassName());
-            addFieldData("method",info.getMethodName());
+            addFieldData("file", info.getFileName());
+            addFieldData("line_number", info.getLineNumber());
+            addFieldData("class", info.getClassName());
+            addFieldData("method", info.getMethodName());
         }
 
-        addFieldData("mdc",mdc);
-        addFieldData("ndc",ndc);
-        addFieldData("level",loggingEvent.getLevel().toString());
+        addFieldData("mdc", mdc);
+        addFieldData("ndc", ndc);
+        addFieldData("level", loggingEvent.getLevel().toString());
 
-        logstashEvent.put("@fields",fieldData);
+        logstashEvent.put("@fields", fieldData);
         return logstashEvent.toString() + "\n";
     }
 
@@ -121,7 +121,7 @@ public class JSONEventLayout extends Layout {
      *
      * @return true if location information is included in log messages, false otherwise.
      */
-    public boolean getLocationInfo(){
+    public boolean getLocationInfo() {
         return locationInfo;
     }
 
@@ -130,7 +130,7 @@ public class JSONEventLayout extends Layout {
      *
      * @param locationInfo true if location information should be included, false otherwise.
      */
-    public void setLocationInfo(boolean locationInfo){
+    public void setLocationInfo(boolean locationInfo) {
         this.locationInfo = locationInfo;
     }
 
@@ -138,8 +138,8 @@ public class JSONEventLayout extends Layout {
         activeIgnoreThrowable = ignoreThrowable;
     }
 
-    private void addFieldData(String keyname, Object keyval){
-        if(null != keyval){
+    private void addFieldData(String keyname, Object keyval) {
+        if (null != keyval) {
             fieldData.put(keyname, keyval);
         }
     }
