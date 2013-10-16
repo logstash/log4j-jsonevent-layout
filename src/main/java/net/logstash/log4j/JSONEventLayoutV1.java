09227daba028e65ca0cfb96bddef48fe87716653
Yanis Guenane <yguenane@gmail.com>
2013-10-16 16:27:45 -0400

    Issue #21 Changes to adapt for @joekiller request

---
+++ src/main/java/net/logstash/log4j/JSONEventLayoutV1.java
@@ -0,0 +1,146 @@
+package net.logstash.log4j;
+
+import net.logstash.log4j.data.HostData;
+import net.minidev.json.JSONObject;
+import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang.time.FastDateFormat;
+import org.apache.log4j.Layout;
+import org.apache.log4j.spi.LocationInfo;
+import org.apache.log4j.spi.LoggingEvent;
+import org.apache.log4j.spi.ThrowableInformation;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.TimeZone;
+
+public class JSONEventLayoutV1 extends Layout {
+
+    private boolean locationInfo = false;
+
+    private String tags;
+    private boolean ignoreThrowable = false;
+
+    private boolean activeIgnoreThrowable = ignoreThrowable;
+    private String hostname = new HostData().getHostName();
+    private String threadName;
+    private long timestamp;
+    private String ndc;
+    private Map mdc;
+    private LocationInfo info;
+    private HashMap<String, Object> exceptionInformation;
+    private static Integer version = 1;
+
+    private JSONObject logstashEvent;
+
+    public static final TimeZone UTC = TimeZone.getTimeZone("UTC");
+    public static final FastDateFormat ISO_DATETIME_TIME_ZONE_FORMAT_WITH_MILLIS = FastDateFormat.getInstance("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'", UTC);
+
+    public static String dateFormat(long timestamp) {
+        return ISO_DATETIME_TIME_ZONE_FORMAT_WITH_MILLIS.format(timestamp);
+    }
+
+    /**
+     * For backwards compatibility, the default is to generate location information
+     * in the log messages.
+     */
+    public JSONEventLayoutV1() {
+        this(true);
+    }
+
+    /**
+     * Creates a layout that optionally inserts location information into log messages.
+     *
+     * @param locationInfo whether or not to include location information in the log messages.
+     */
+    public JSONEventLayoutV1(boolean locationInfo) {
+        this.locationInfo = locationInfo;
+    }
+
+    public String format(LoggingEvent loggingEvent) {
+        threadName = loggingEvent.getThreadName();
+        timestamp = loggingEvent.getTimeStamp();
+        exceptionInformation = new HashMap<String, Object>();
+        mdc = loggingEvent.getProperties();
+        ndc = loggingEvent.getNDC();
+
+        logstashEvent = new JSONObject();
+
+        /**
+         * All v1 of the event format requires is
+         * "@timestamp" and "@version"
+         * Every other field is arbitrary
+         */
+        logstashEvent.put("@version", version);
+        logstashEvent.put("@timestamp", dateFormat(timestamp));
+
+        /**
+         * Now we start injecting our own stuff.
+         */
+        logstashEvent.put("source_host", hostname);
+        logstashEvent.put("message", loggingEvent.getRenderedMessage());
+
+        if (loggingEvent.getThrowableInformation() != null) {
+            final ThrowableInformation throwableInformation = loggingEvent.getThrowableInformation();
+            if (throwableInformation.getThrowable().getClass().getCanonicalName() != null) {
+                exceptionInformation.put("exception_class", throwableInformation.getThrowable().getClass().getCanonicalName());
+            }
+            if (throwableInformation.getThrowable().getMessage() != null) {
+                exceptionInformation.put("exception_message", throwableInformation.getThrowable().getMessage());
+            }
+            if (throwableInformation.getThrowableStrRep() != null) {
+                String stackTrace = StringUtils.join(throwableInformation.getThrowableStrRep(), "\n");
+                exceptionInformation.put("stacktrace", stackTrace);
+            }
+            addEventData("exception", exceptionInformation);
+        }
+
+        if (locationInfo) {
+            info = loggingEvent.getLocationInformation();
+            addEventData("file", info.getFileName());
+            addEventData("line_number", info.getLineNumber());
+            addEventData("class", info.getClassName());
+            addEventData("method", info.getMethodName());
+        }
+
+        addEventData("logger_name", loggingEvent.getLoggerName());
+        addEventData("mdc", mdc);
+        addEventData("ndc", ndc);
+        addEventData("level", loggingEvent.getLevel().toString());
+        addEventData("thread_name", threadName);
+
+        return logstashEvent.toString() + "\n";
+    }
+
+    public boolean ignoresThrowable() {
+        return ignoreThrowable;
+    }
+
+    /**
+     * Query whether log messages include location information.
+     *
+     * @return true if location information is included in log messages, false otherwise.
+     */
+    public boolean getLocationInfo() {
+        return locationInfo;
+    }
+
+    /**
+     * Set whether log messages should include location information.
+     *
+     * @param locationInfo true if location information should be included, false otherwise.
+     */
+    public void setLocationInfo(boolean locationInfo) {
+        this.locationInfo = locationInfo;
+    }
+
+    public void activateOptions() {
+        activeIgnoreThrowable = ignoreThrowable;
+    }
+
+    private void addEventData(String keyname, Object keyval) {
+        if (null != keyval) {
+            logstashEvent.put(keyname, keyval);
+        }
+    }
+}
---
+++ src/test/java/net/logstash/log4j/JSONEventV1LayoutTest.java
@@ -0,0 +1,187 @@
+package net.logstash.log4j;
+
+import junit.framework.Assert;
+import net.minidev.json.JSONObject;
+import net.minidev.json.JSONValue;
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+import org.apache.log4j.NDC;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Ignore;
+import org.junit.Test;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: jvincent
+ * Date: 12/5/12
+ * Time: 12:07 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class JSONEventV1LayoutTest {
+    static Logger logger;
+    static MockAppenderV1 appender;
+    static final String[] logstashFields = new String[]{
+            "message",
+            "source_host",
+            "@timestamp",
+            "@version"
+    };
+
+    @BeforeClass
+    public static void setupTestAppender() {
+        appender = new MockAppenderV1(new JSONEventLayoutV1());
+        logger = Logger.getRootLogger();
+        appender.setThreshold(Level.TRACE);
+        appender.setName("mockappender");
+        appender.activateOptions();
+        logger.addAppender(appender);
+    }
+
+    @After
+    public void clearTestAppender() {
+        NDC.clear();
+        appender.clear();
+        appender.close();
+    }
+
+    @Test
+    public void testJSONEventLayoutIsJSON() {
+        logger.info("this is an info message");
+        String message = appender.getMessages()[0];
+        Assert.assertTrue("Event is not valid JSON", JSONValue.isValidJsonStrict(message));
+    }
+
+    @Test
+    public void testJSONEventLayoutHasKeys() {
+        logger.info("this is a test message");
+        String message = appender.getMessages()[0];
+        Object obj = JSONValue.parse(message);
+        JSONObject jsonObject = (JSONObject) obj;
+        for (String fieldName : logstashFields) {
+            Assert.assertTrue("Event does not contain field: " + fieldName, jsonObject.containsKey(fieldName));
+        }
+    }
+
+    @Test
+    public void testJSONEventLayoutHasNDC() {
+        String ndcData = new String("json-layout-test");
+        NDC.push(ndcData);
+        logger.warn("I should have NDC data in my log");
+        String message = appender.getMessages()[0];
+        Object obj = JSONValue.parse(message);
+        JSONObject jsonObject = (JSONObject) obj;
+
+        Assert.assertEquals("NDC is wrong", ndcData, jsonObject.get("ndc"));
+    }
+
+    @Test
+    public void testJSONEventLayoutExceptions() {
+        String exceptionMessage = new String("shits on fire, yo");
+        logger.fatal("uh-oh", new IllegalArgumentException(exceptionMessage));
+        String message = appender.getMessages()[0];
+        Object obj = JSONValue.parse(message);
+        JSONObject jsonObject = (JSONObject) obj;
+        JSONObject exceptionInformation = (JSONObject) jsonObject.get("exception");
+
+        Assert.assertEquals("Exception class missing", "java.lang.IllegalArgumentException", exceptionInformation.get("exception_class"));
+        Assert.assertEquals("Exception exception message", exceptionMessage, exceptionInformation.get("exception_message"));
+    }
+
+    @Test
+    public void testJSONEventLayoutHasClassName() {
+        logger.warn("warning dawg");
+        String message = appender.getMessages()[0];
+        Object obj = JSONValue.parse(message);
+        JSONObject jsonObject = (JSONObject) obj;
+
+        Assert.assertEquals("Logged class does not match", this.getClass().getCanonicalName().toString(), jsonObject.get("class"));
+    }
+
+    @Test
+    public void testJSONEventHasFileName() {
+        logger.warn("whoami");
+        String message = appender.getMessages()[0];
+        Object obj = JSONValue.parse(message);
+        JSONObject jsonObject = (JSONObject) obj;
+
+        Assert.assertNotNull("File value is missing", jsonObject.get("file"));
+    }
+
+    @Test
+    public void testJSONEventHasLoggerName() {
+        logger.warn("whoami");
+        String message = appender.getMessages()[0];
+        Object obj = JSONValue.parse(message);
+        JSONObject jsonObject = (JSONObject) obj;
+        Assert.assertNotNull("LoggerName value is missing", jsonObject.get("logger_name"));
+    }
+
+    @Test
+    public void testJSONEventHasThreadName() {
+        logger.warn("whoami");
+        String message = appender.getMessages()[0];
+        Object obj = JSONValue.parse(message);
+        JSONObject jsonObject = (JSONObject) obj;
+        Assert.assertNotNull("ThreadName value is missing", jsonObject.get("thread_name"));
+    }
+
+    @Test
+    public void testJSONEventLayoutNoLocationInfo() {
+        JSONEventLayoutV1 layout = (JSONEventLayoutV1) appender.getLayout();
+        boolean prevLocationInfo = layout.getLocationInfo();
+
+        layout.setLocationInfo(false);
+
+        logger.warn("warning dawg");
+        String message = appender.getMessages()[0];
+        Object obj = JSONValue.parse(message);
+        JSONObject jsonObject = (JSONObject) obj;
+
+        Assert.assertFalse("atFields contains file value", jsonObject.containsKey("file"));
+        Assert.assertFalse("atFields contains line_number value", jsonObject.containsKey("line_number"));
+        Assert.assertFalse("atFields contains class value", jsonObject.containsKey("class"));
+        Assert.assertFalse("atFields contains method value", jsonObject.containsKey("method"));
+
+        // Revert the change to the layout to leave it as we found it.
+        layout.setLocationInfo(prevLocationInfo);
+    }
+
+    @Test
+    @Ignore
+    public void measureJSONEventLayoutLocationInfoPerformance() {
+        JSONEventLayoutV1 layout = (JSONEventLayoutV1) appender.getLayout();
+        boolean locationInfo = layout.getLocationInfo();
+        int iterations = 100000;
+        long start, stop;
+
+        start = System.currentTimeMillis();
+        for (int i = 0; i < iterations; i++) {
+            logger.warn("warning dawg");
+        }
+        stop = System.currentTimeMillis();
+        long firstMeasurement = stop - start;
+
+        layout.setLocationInfo(!locationInfo);
+        start = System.currentTimeMillis();
+        for (int i = 0; i < iterations; i++) {
+            logger.warn("warning dawg");
+        }
+        stop = System.currentTimeMillis();
+        long secondMeasurement = stop - start;
+
+        System.out.println("First Measurement (locationInfo: " + locationInfo + "): " + firstMeasurement);
+        System.out.println("Second Measurement (locationInfo: " + !locationInfo + "): " + secondMeasurement);
+
+        // Clean up
+        layout.setLocationInfo(!locationInfo);
+    }
+
+    @Test
+    public void testDateFormat() {
+        long timestamp = 1364844991207L;
+        Assert.assertEquals("format does not produce expected output", "2013-04-01T19:36:31.207Z", JSONEventLayoutV1.dateFormat(timestamp));
+    }
+}
